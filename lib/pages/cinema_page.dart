f1716d541454b0ee386c87cc794dba23cfaf7edd
Leonardo <leonardo.santos1095@etec.sp.gov.br>
2024-08-14 22:50:59 -0300

    branch com o código da aula do dia Wed, Aug 14, 2024 10:50:59 PM

---
+++ lib/pages/cinema_page.dart
@@ -0,0 +1,65 @@
+import 'package:cinedobrada/componentes/cinema_item.dart';
+import 'package:cinedobrada/componentes/mapa.dart';
+import 'package:cinedobrada/vm/cinema_vm.dart';
+import 'package:cinedobrada/vm/filme_vm.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CinemaPage extends StatelessWidget {
+  const CinemaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = Provider.of<CinemaViewModel>(context);
+    final cinemas = vm.cinemas;
+
+    final screensize = MediaQuery.of(context).size;
+    final listheight = screensize.height * 0.4;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cinemas"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black,
+              width: double.maxFinite,
+              child: Mapa(
+                cinemas: cinemas,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: double.maxFinite,
+            height: listheight,
+            child: ListView.builder(
+              itemCount: cinemas.length,
+              itemBuilder: (context, index) => GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  final filmeVm = Provider.of<FilmesViewModel>(
+                    context,
+                    listen: false,
+                  );
+                  filmeVm.useCinema(cinemas[index].id);
+
+                  Navigator.pushNamed(
+                    context,
+                    "/filmes",
+                    arguments: cinemas[index].id,
+                  );
+                },
+                child: CinemaItem(
+                  cinema: cinemas[index],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/vm/filme_vm.dart
@@ -0,0 +1,24 @@
+import 'package:cinedobrada/entidades/filme.dart';
+import 'package:cinedobrada/repositorio/repositorio_filme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class FilmesViewModel extends ChangeNotifier {
+  late List<Filmes> filmes;
+
+  FilmesViewModel useLista(List<Filmes> filmes) {
+    this.filmes = filmes;
+    notifyListeners();
+    return this;
+  }
+
+  void useCinema(String id) {
+    filmes = RepositorioFilmes().select(id);
+    notifyListeners();
+  }
+
+  static ChangeNotifierProvider<FilmesViewModel> novo() =>
+      ChangeNotifierProvider(
+        create: (_) => FilmesViewModel().useLista([]),
+      );
+}
